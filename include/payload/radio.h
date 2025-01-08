920b8812dd628a9f89b958a4a30bcbd68ac301b4
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-01-08 14:16:56 -0300

    feat: Implementação o rádio LoRa da carga do balão. Criação do enum e struct para tipos de mensagens e mensagens do rádio.

--- include/payload/radio.h
+++ include/payload/radio.h
@@ -3,13 +3,11 @@
 #include "esp_err.h"
 #include "freertos/event_groups.h"
 #include "freertos/queue.h"
+#include "message_types.h"
 
 #define RADIO_EV_TRANSMITTED (1<<0)
 
 EventGroupHandle_t radio_event_group;
 
 esp_err_t radio_init();
-esp_err_t radio_start_receiving();
-esp_err_t radio_standby();
-esp_err_t radio_receive_data();
-void radio_get_status();
+esp_err_t radio_transmit_data(radio_message_t *radio_message);
--- src/payload/radio.cpp
+++ src/payload/radio.cpp
@@ -5,3 +5,35 @@
 #include "payload/radio.h"
 #include "payload/config.h"
 
+LoRa_E220 radio(&Serial2, LORA_AUX_PIN, LORA_M0_PIN, LORA_M1_PIN);
+
+esp_err_t radio_init() {
+    esp_err_t ret = ESP_OK;
+
+    if (!radio.begin()) {
+        Serial.printf("Falha ao iniciar comunicação com controlador LoRa.\n");
+        return ESP_FAIL;
+    }
+    
+    ResponseStructContainer device_data = radio.getConfiguration();
+    Configuration config = *(Configuration*) device_data.data;
+    // XXX Mexer nas configurações (talvez?)
+    config.TRANSMISSION_MODE.enableRSSI = RSSI_ENABLED;
+    device_data.close();
+
+    return ESP_OK;
+}
+
+esp_err_t radio_transmit(radio_message_t *radio_message) {
+    esp_err_t ret = ESP_OK;
+    
+    ResponseStatus rs = radio.sendBroadcastFixedMessage(LORA_CHANNEL, radio_message, sizeof(radio_message));
+
+    ESP_GOTO_ON_FALSE(rs.code == E220_SUCCESS, ESP_FAIL, err, "LoRa Radio", "Falha ao transmitir mensagem via rádio.");
+
+    return ESP_OK;
+err:
+    // XXX Pânico
+    Serial.printf("Falha ao transmitir mensagem via rádio, status: %d\n", rs.code);
+    return ret;
+}
