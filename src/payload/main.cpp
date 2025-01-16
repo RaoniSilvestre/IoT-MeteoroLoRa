68498aa2bf9977804fb91300cda2b7e64b1d5300
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-01-16 14:48:59 -0300

    feat: Código de teste do termometro

--- src/payload/main.cpp
+++ src/payload/main.cpp
@@ -1,27 +1,29 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "payload/magneto.h"
+#include "payload/termo.h"
 #include "payload/config.h"
 
-magneto_data_t *data = (magneto_data_t*)malloc(sizeof(magneto_data_t));
-float azimuth;
 
+magneto_data_t *mag_data = (magneto_data_t*)malloc(sizeof(magneto_data_t));
+float azimuth;
+termo_data_t *termo_data = (termo_data_t*)malloc(sizeof(termo_data_t));
+/*
 void magneto_task(void *args) {
     
 
     for (;;) {
-        if (magneto_read_data(data) == ESP_OK) {
-            azimuth = atan2(data->y,data->x) * 180.0/PI;
+        if (magneto_read_data(mag_data) == ESP_OK) {
+            azimuth = atan2(mag_data->y,mag_data->x) * 180.0/PI;
             azimuth += MAG_DECLINATION_ANGLE;
             azimuth += azimuth < 0 ? 360 : 0;
 
-
-            Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", data->x, data->y, data->z, azimuth);
+            Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", mag_data->x, mag_data->y, mag_data->z, azimuth);
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
     vTaskDelete(NULL);
-}
+}*/
 
 void setup() {
     Serial.begin(115200);
@@ -29,17 +31,22 @@ void setup() {
     if (magneto_init() != ESP_OK) {
         Serial.println("Falha ao inicializar o magneto");
     }
+    if (termo_init() != ESP_OK) {
+        Serial.println("Falha ao inicializar o termometro");
+    }
     //xTaskCreate(magneto_task, "Magneto Task", 1024, NULL, 5, NULL);
 }
 
 void loop() {
-    if (magneto_read_data(data) == ESP_OK) {
-        azimuth = atan2(data->y,data->x) * 180.0/PI;
+    if (magneto_read_data(mag_data) == ESP_OK) {
+        azimuth = atan2(mag_data->y,mag_data->x) * 180.0/PI;
         azimuth += MAG_DECLINATION_ANGLE;
         azimuth += azimuth < 0 ? 360 : 0;
 
-
-        Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", data->x, data->y, data->z, azimuth);
+        Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", mag_data->x, mag_data->y, mag_data->z, azimuth);
+    }
+    if (termo_read_data(termo_data) == ESP_OK) {
+        Serial.printf("Temperatura: %.2fºC, Umidade: %.2f%%\n", termo_data->temp, termo_data->humd);
     }
     vTaskDelay(pdMS_TO_TICKS(100));
 }
