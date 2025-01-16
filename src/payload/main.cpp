36f535b0975d9017296de49a6b6d4f122c33f152
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-01-16 12:11:58 -0300

    feat: Código de teste para o magneto funcionando, exemplo de task, mas que não funciona.

--- src/payload/main.cpp
+++ src/payload/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "payload/magneto.h"
+#include "payload/config.h"
+
+magneto_data_t *data = (magneto_data_t*)malloc(sizeof(magneto_data_t));
+float azimuth;
+
+void magneto_task(void *args) {
+    
+
+    for (;;) {
+        if (magneto_read_data(data) == ESP_OK) {
+            azimuth = atan2(data->y,data->x) * 180.0/PI;
+            azimuth += MAG_DECLINATION_ANGLE;
+            azimuth += azimuth < 0 ? 360 : 0;
+
+
+            Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", data->x, data->y, data->z, azimuth);
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+    vTaskDelete(NULL);
+}
 
 void setup() {
-  
+    Serial.begin(115200);
+    Wire.begin();
+    if (magneto_init() != ESP_OK) {
+        Serial.println("Falha ao inicializar o magneto");
+    }
+    //xTaskCreate(magneto_task, "Magneto Task", 1024, NULL, 5, NULL);
 }
 
 void loop() {
-  
+    if (magneto_read_data(data) == ESP_OK) {
+        azimuth = atan2(data->y,data->x) * 180.0/PI;
+        azimuth += MAG_DECLINATION_ANGLE;
+        azimuth += azimuth < 0 ? 360 : 0;
+
+
+        Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", data->x, data->y, data->z, azimuth);
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
 }
