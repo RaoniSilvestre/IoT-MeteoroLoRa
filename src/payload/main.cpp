8648e86e2f9f82549d820d93a803633f1472c7bd
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-01-17 10:52:49 -0300

    fix: Resolver problema de leitura no código de exemplo

--- src/payload/main.cpp
+++ src/payload/main.cpp
@@ -41,7 +41,7 @@ void loop() {
     int16_t x = 0, y = 0, z = 0;
     float temp = 0, humd = 0;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
         if (magneto_read_data(mag_data) == ESP_OK) {
             x += mag_data->x;
             y += mag_data->y;
@@ -51,17 +51,20 @@ void loop() {
             temp += termo_data->temp;
             humd += termo_data->humd;
         }
-        vTaskDelay(pdMS_TO_TICKS(10));
+        //vTaskDelay(pdMS_TO_TICKS(100));
     }
-    mag_data->x = x / 10;
-    mag_data->y = y / 10;
-    mag_data->z = z / 10;
-    termo_data->temp = temp / 10;
-    termo_data->humd = humd / 10;
+    mag_data->x = x / 100;
+    mag_data->y = y / 100;
+    mag_data->z = z / 100;
+
+    termo_data->temp = temp / 100;
+    termo_data->humd = humd / 100;
+
     azimuth = atan2(mag_data->y,mag_data->x) * 180.0/PI;
     azimuth += MAG_DECLINATION_ANGLE;
     azimuth += azimuth < 0 ? 360 : 0;
-    Serial.printf("X: %d, Y: %d, Z: %d, Heading: %.2fº\n", mag_data->x, mag_data->y, mag_data->z, azimuth);
-    Serial.printf("Temperatura: %.2fºC, Umidade: %.2f%%\n", termo_data->temp, termo_data->humd);
-    vTaskDelay(pdMS_TO_TICKS(100));
+
+    Serial.printf("X: %dmT, Y: %dmT, Z: %dmT, Heading: %.2fº\n", mag_data->x, mag_data->y, mag_data->z, azimuth);
+    Serial.printf("Temp: %.2fºC, Umid: %.2f%%\n", termo_data->temp, termo_data->humd);
+    //vTaskDelay(pdMS_TO_TICKS(10));
 }
