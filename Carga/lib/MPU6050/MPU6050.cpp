f5afaaee71871cc61dd49f37ddc0c8e87be2cd4f
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-04-03 10:21:59 -0300

    feat: Refatoração da biblioteca do mpu6050. Uso de structs e enums para modelagem do sensor e suas configurações. Funções básicas de calibração (temporárias devido a baixa precisão).

--- Carga/lib/MPU6050/MPU6050.cpp
+++ Carga/lib/MPU6050/MPU6050.cpp
@@ -14,6 +14,17 @@
 #define MPU6050_REG_ACCEL_CONFIG 0x1C  // Configuração do acelerômetro
 #define MPU6050_REG_FIFO_EN 0x23       // Habilita buffer FIFO
 
+// Configuração de escala
+#define MPU6050_GYRO_CONFIG_FS_250 0x00  // ±250°/s
+#define MPU6050_GYRO_CONFIG_FS_500 0x08  // ±500°/s
+#define MPU6050_GYRO_CONFIG_FS_1000 0x10 // ±1000°/s
+#define MPU6050_GYRO_CONFIG_FS_2000 0x18 // ±2000°/s
+
+#define MPU6050_ACCEL_CONFIG_FS_2G 0x00  // ±2g
+#define MPU6050_ACCEL_CONFIG_FS_4G 0x08  // ±4g
+#define MPU6050_ACCEL_CONFIG_FS_8G 0x10  // ±8g
+#define MPU6050_ACCEL_CONFIG_FS_16G 0x18 // ±16g
+
 // Dados dos sensores
 #define MPU6050_REG_ACCEL_XOUT_MSB 0x3B
 #define MPU6050_REG_ACCEL_XOUT_LSB 0x3C
@@ -56,94 +67,243 @@
 #define MPU6050_REG_ZG_OFFSET_MSB 0x17
 #define MPU6050_REG_ZG_OFFSET_LSB 0x18
 
-esp_err_t mpu6050_init() {
-    if (mpu6050_pwr_mgmt_1_mode(0x00, 0x00, 0x00, MPU6050_PWR_MGMT_1_CLKSEL_PLL_X) != ESP_OK) {
-        Serial.println("[MPU6050 (Acel/Giro)] Falha ao configurar o modo de energia");
-        return ESP_FAIL;
-    }
-    if (mpu6050_set_filter(MPU6050_FILTER_CONFIG_5HZ) != ESP_OK) {
-        Serial.println("[MPU6050 (Acel/Giro)] Falha ao configurar o filtro");
-        return ESP_FAIL;
-    }
-    if (mpu6050_accel_set_config(MPU6050_ACCEL_CONFIG_FS_4G) != ESP_OK) {
-        Serial.println("[MPU6050 (Acel/Giro)] Falha ao configurar o acelerômetro");
-        return ESP_FAIL;
-    }
-    if (mpu6050_gyro_set_config(MPU6050_GYRO_CONFIG_FS_500) != ESP_OK) {
-        Serial.println("[MPU6050 (Acel/Giro)] Falha ao configurar o giroscópio");
-        return ESP_FAIL;
-    }
+esp_err_t mpu6050_init(mpu6050_dev_t *dev) {
+    
     return ESP_OK;
 }
 
-esp_err_t mpu6050_set_filter(uint8_t filter_config) {
+esp_err_t mpu6050_set_filter(mpu6050_dev_t *dev, mpu6050_filter_config_t filter_config) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (filter_config > MPU6050_CONFIG_FILTER_5HZ || filter_config < MPU6050_CONFIG_FILTER_260HZ) {
+        Serial.println("[MPU6050 (Acel/Giro)] Filtro inválido");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     Serial.printf("[MPU6050 (Acel/Giro)] Configurando filtro: 0x%02X\n", filter_config);
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_FILTER_CONFIG, filter_config);
+    dev->filter_config = filter_config;
+    // Configura o filtro no registrador de configuração do filtro
+    return sensor_write_reg(dev->addr, MPU6050_REG_FILTER_CONFIG, filter_config);
 }
 
-esp_err_t mpu6050_soft_reset() {
+esp_err_t mpu6050_soft_reset(mpu6050_dev_t *dev) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     Serial.println("[MPU6050 (Acel/Giro)] Resetando caminho de sinal");
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_SIG_PATH_RESET, MPU6050_SIG_PATH_RESET_TEMP_RST | MPU6050_SIG_PATH_RESET_ACCEL_RST | MPU6050_SIG_PATH_RESET_GYRO_RST);
+    return sensor_write_reg(dev->addr, MPU6050_REG_SIG_PATH_RESET, MPU6050_SIG_PATH_RESET_TEMP_RST | MPU6050_SIG_PATH_RESET_ACCEL_RST | MPU6050_SIG_PATH_RESET_GYRO_RST);
 }
 
-esp_err_t mpu6050_hard_reset() {
+esp_err_t mpu6050_hard_reset(mpu6050_dev_t *dev) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     Serial.println("[MPU6050 (Acel/Giro)] Resetando o dispositivo");
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_PWR_MGMT_1, MPU6050_PWR_MGMT_1_DEVICE_RESET);
+    return sensor_write_reg(dev->addr, MPU6050_REG_PWR_MGMT_1, MPU6050_PWR_MGMT_1_DEVICE_RESET);
 }
 
-esp_err_t mpu6050_pwr_mgmt_1_mode(uint8_t sleep, uint8_t cycle, uint8_t temp_dis, uint8_t clk_sel) {
+esp_err_t mpu6050_pwr_mgmt_1_mode(mpu6050_dev_t *dev, uint8_t sleep, uint8_t cycle, uint8_t temp_dis, uint8_t clk_sel) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     uint8_t pwr_mgmt_1 = sleep | cycle | temp_dis | clk_sel;
     Serial.printf("[MPU6050 (Acel/Giro)] Configurando modo de energia: 0x%02X\n", pwr_mgmt_1);
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_PWR_MGMT_1, pwr_mgmt_1);
+    return sensor_write_reg(dev->addr, MPU6050_REG_PWR_MGMT_1, pwr_mgmt_1);
 }
 
-esp_err_t mpu6050_pwr_mgmt_2_mode(uint8_t lp_wake_ctrl, uint8_t stby_xa, uint8_t stby_ya, uint8_t stby_za, uint8_t stby_xg, uint8_t stby_yg, uint8_t stby_zg) {
+esp_err_t mpu6050_pwr_mgmt_2_mode(mpu6050_dev_t *dev, uint8_t lp_wake_ctrl, uint8_t stby_xa, uint8_t stby_ya, uint8_t stby_za, uint8_t stby_xg, uint8_t stby_yg, uint8_t stby_zg) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     uint8_t pwr_mgmt_2 = lp_wake_ctrl | stby_xa | stby_ya | stby_za | stby_xg | stby_yg | stby_zg;
     Serial.printf("[MPU6050 (Acel/Giro)] Configurando modo de energia 2: 0x%02X\n", pwr_mgmt_2);
     return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_PWR_MGMT_2, pwr_mgmt_2);
 }
 
-esp_err_t mpu6050_accel_set_config(uint8_t accel_fs) {
+esp_err_t mpu6050_accel_set_config(mpu6050_dev_t *dev, mpu6050_accel_fs_t accel_fs) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (accel_fs > MPU6050_CONFIG_AFS3 || accel_fs < MPU6050_CONFIG_AFS0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Configuração de acelerômetro inválida");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (accel_fs == dev->accel_fs) {
+        Serial.println("[MPU6050 (Acel/Giro)] Acelerômetro já configurado");
+        return ESP_OK;
+    }
+
+    dev->accel_fs = accel_fs;
+    uint8_t accel_fs_config = (accel_fs << 3) & 0x18; // Configuração do acelerômetro
     Serial.printf("[MPU6050 (Acel/Giro)] Configurando acelerômetro: 0x%02X\n", accel_fs);
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_ACCEL_CONFIG, accel_fs);
+    return sensor_write_reg(dev->addr, MPU6050_REG_ACCEL_CONFIG, accel_fs_config);
 }
 
-esp_err_t mpu6050_gyro_set_config(uint8_t gyro_fs) {
+esp_err_t mpu6050_gyro_set_config(mpu6050_dev_t *dev, mpu6050_gyro_fs_t gyro_fs) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (gyro_fs > MPU6050_CONFIG_GFS3 || gyro_fs < MPU6050_CONFIG_GFS0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Configuração de giroscópio inválida");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (gyro_fs == dev->gyro_fs) {
+        Serial.println("[MPU6050 (Acel/Giro)] Giroscópio já configurado");
+        return ESP_OK;
+    }
+
+    dev->gyro_fs = gyro_fs;
+    uint8_t gyro_fs_config = (gyro_fs << 3) & 0x18; // Configuração do giroscópio
     Serial.printf("[MPU6050 (Acel/Giro)] Configurando giroscópio: 0x%02X\n", gyro_fs);
-    return sensor_write_reg(MPU6050_ADDRESS, MPU6050_REG_GYRO_CONFIG, gyro_fs);
+    return sensor_write_reg(dev->addr, MPU6050_REG_GYRO_CONFIG, gyro_fs_config);
 }
 
-esp_err_t mpu6050_accel_read_data(accel_data_t *data) {
+esp_err_t mpu6050_calibrate_accel(mpu6050_dev_t *dev, uint16_t samples) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (samples == 0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Número de amostras inválido");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    accel_data_t data = {0};
+    for (uint16_t i = 0; i < samples; i++) {
+        mpu6050_accel_read_data(dev, &data);
+        dev->accel_offset.x += data.x;
+        dev->accel_offset.y += data.y;
+        dev->accel_offset.z += data.z;
+        delay(10);
+    }
+    dev->accel_offset.x /= samples;
+    dev->accel_offset.y /= samples;
+    dev->accel_offset.z /= samples;
+
+    Serial.printf("[MPU6050 (Acel/Giro)] Acelerômetro calibrado: x=%.2f, y=%.2f, z=%.2f\n", dev->accel_offset.x, dev->accel_offset.y, dev->accel_offset.z);
+    return ESP_OK;
+}
+
+esp_err_t mpu6050_calibrate_gyro(mpu6050_dev_t *dev, uint16_t samples) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (samples == 0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Número de amostras inválido");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    gyro_data_t data;
+    for (uint16_t i = 0; i < samples; i++) {
+        mpu6050_gyro_read_data(dev, &data);
+        dev->gyro_offset.x += data.x;
+        dev->gyro_offset.y += data.y;
+        dev->gyro_offset.z += data.z;
+        delay(10);
+    }
+    dev->gyro_offset.x /= samples;
+    dev->gyro_offset.y /= samples;
+    dev->gyro_offset.z /= samples;
+
+    Serial.printf("[MPU6050 (Acel/Giro)] Giroscópio calibrado: x=%.2f, y=%.2f, z=%.2f\n", dev->gyro_offset.x, dev->gyro_offset.y, dev->gyro_offset.z);
+    return ESP_OK;
+}
+
+esp_err_t mpu6050_accel_read_data(mpu6050_dev_t *dev, accel_data_t *data) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (data == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dados do acelerômetro não inicializados");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (dev->accel_fs > MPU6050_CONFIG_AFS3 || dev->accel_fs < MPU6050_CONFIG_AFS0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Configuração de acelerômetro inválida");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     uint8_t buffer[6];
-    if (sensor_read_reg(MPU6050_ADDRESS, MPU6050_REG_ACCEL_XOUT_MSB, buffer, 6) != ESP_OK) {
+    if (sensor_read_reg(dev->addr, MPU6050_REG_ACCEL_XOUT_MSB, buffer, 6) != ESP_OK) {
         Serial.println("[MPU6050 (Acel/Giro)] Falha ao ler dados do acelerômetro");
         return ESP_FAIL;
     }
+
+    uint16_t scale = (1 << (14 - dev->accel_fs));
     int16_t raw = (int16_t)(buffer[0] << 8 | buffer[1]);
-    data->x = (float)raw;
+    data->x = (float)raw / scale - dev->accel_offset.x;
     raw = (int16_t)(buffer[2] << 8 | buffer[3]);
-    data->y = (float)raw;
+    data->y = (float)raw / scale - dev->accel_offset.y;
     raw = (int16_t)(buffer[4] << 8 | buffer[5]);
-    data->z = (float)raw; 
+    data->z = (float)raw / scale - dev->accel_offset.z;
     return ESP_OK;
 }
 
-esp_err_t mpu6050_gyro_read_data(gyro_data_t *data) {
+esp_err_t mpu6050_gyro_read_data(mpu6050_dev_t *dev, gyro_data_t *data) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (data == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dados do giroscópio não inicializados");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (dev->gyro_fs > MPU6050_CONFIG_GFS3 || dev->gyro_fs < MPU6050_CONFIG_GFS0) {
+        Serial.println("[MPU6050 (Acel/Giro)] Configuração de giroscópio inválida");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     uint8_t buffer[6];
-    if (sensor_read_reg(MPU6050_ADDRESS, MPU6050_REG_GYRO_XOUT_MSB, buffer, 6) != ESP_OK) {
+    if (sensor_read_reg(dev->addr, MPU6050_REG_GYRO_XOUT_MSB, buffer, 6) != ESP_OK) {
         Serial.println("[MPU6050 (Acel/Giro)] Falha ao ler dados do giroscópio");
         return ESP_FAIL;
     }
+
+    float scale = (1 << (17 - dev->gyro_fs)) / 1000.0f;
     int16_t raw = (int16_t)(buffer[0] << 8 | buffer[1]);
-    data->x = (float)raw;
+    data->x = (float)raw / scale - dev->gyro_offset.x;
     raw = (int16_t)(buffer[2] << 8 | buffer[3]);
-    data->y = (float)raw;
+    data->y = (float)raw / scale - dev->gyro_offset.y;
     raw = (int16_t)(buffer[4] << 8 | buffer[5]);
-    data->z = (float)raw;
+    data->z = (float)raw / scale - dev->gyro_offset.z;
     return ESP_OK;
 }
 
-esp_err_t mpu6050_read_temp(int16_t *temp) {
+esp_err_t mpu6050_read_temp(mpu6050_dev_t *dev, int16_t *temp) {
+    if (dev == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dispositivo não inicializado");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    if (temp == NULL) {
+        Serial.println("[MPU6050 (Acel/Giro)] Dados de temperatura não inicializados");
+        return ESP_ERR_INVALID_ARG;
+    }
+
     uint8_t buffer[2];
     if (sensor_read_reg(MPU6050_ADDRESS, MPU6050_REG_TEMP_OUT_MSB, buffer, 2) != ESP_OK) {
         Serial.println("[MPU6050 (Acel/Giro)] Falha ao ler temperatura");
--- Carga/lib/MPU6050/MPU6050.h
+++ Carga/lib/MPU6050/MPU6050.h
@@ -5,16 +5,6 @@
 
 #define MPU6050_ADDRESS 0x68
 
-#define MPU6050_GYRO_CONFIG_FS_250 0x00  // ±250°/s
-#define MPU6050_GYRO_CONFIG_FS_500 0x08  // ±500°/s
-#define MPU6050_GYRO_CONFIG_FS_1000 0x10 // ±1000°/s
-#define MPU6050_GYRO_CONFIG_FS_2000 0x18 // ±2000°/s
-
-#define MPU6050_ACCEL_CONFIG_FS_2G 0x00  // ±2g
-#define MPU6050_ACCEL_CONFIG_FS_4G 0x08  // ±4g
-#define MPU6050_ACCEL_CONFIG_FS_8G 0x10  // ±8g
-#define MPU6050_ACCEL_CONFIG_FS_16G 0x18 // ±16g
-
 // Bits de configuração do PWR_MGMT_1
 #define MPU6050_PWR_MGMT_1_DEVICE_RESET 0x80 // Reseta todos os registradores
 #define MPU6050_PWR_MGMT_1_SLEEP                                               \
@@ -37,17 +27,33 @@
 #define MPU6050_PWR_MGMT_2_STBY_YG 0x02 // Desativa o giroscópio no eixo Y
 #define MPU6050_PWR_MGMT_2_STBY_ZG 0x01 // Desativa o giroscópio no eixo Z
 
-#define MPU6050_FILTER_CONFIG_260HZ 0x00 // Filtro de 260Hz
-#define MPU6050_FILTER_CONFIG_184HZ 0x01 // Filtro de 184Hz
-#define MPU6050_FILTER_CONFIG_94HZ 0x02  // Filtro de 94Hz
-#define MPU6050_FILTER_CONFIG_44HZ 0x03  // Filtro de 44Hz
-#define MPU6050_FILTER_CONFIG_21HZ 0x04  // Filtro de 21Hz
-#define MPU6050_FILTER_CONFIG_10HZ 0x05  // Filtro de 10Hz
-#define MPU6050_FILTER_CONFIG_5HZ 0x06   // Filtro de 5Hz
-
 #define MPU6050_SIG_PATH_RESET_TEMP_RST 0x01  // Reseta o sensor de temperatura
 #define MPU6050_SIG_PATH_RESET_ACCEL_RST 0x02 // Reseta o acelerômetro
 #define MPU6050_SIG_PATH_RESET_GYRO_RST 0x04  // Reseta o giroscópio
+
+typedef enum {
+    MPU6050_CONFIG_AFS0 = 0, // ±2g
+    MPU6050_CONFIG_AFS1 = 1, // ±4g
+    MPU6050_CONFIG_AFS2 = 2, // ±8g
+    MPU6050_CONFIG_AFS3 = 3, // ±16g
+} mpu6050_accel_fs_t;
+
+typedef enum {
+    MPU6050_CONFIG_GFS0 = 0, // ±250°/s
+    MPU6050_CONFIG_GFS1 = 1, // ±500°/s
+    MPU6050_CONFIG_GFS2 = 2, // ±1000°/s
+    MPU6050_CONFIG_GFS3 = 3, // ±2000°/s
+} mpu6050_gyro_fs_t;
+
+typedef enum {
+    MPU6050_CONFIG_FILTER_260HZ = 0,
+    MPU6050_CONFIG_FILTER_184HZ = 1,
+    MPU6050_CONFIG_FILTER_94HZ = 2,
+    MPU6050_CONFIG_FILTER_44HZ = 3,
+    MPU6050_CONFIG_FILTER_21HZ = 4,
+    MPU6050_CONFIG_FILTER_10HZ = 5,
+    MPU6050_CONFIG_FILTER_5HZ = 6, 
+} mpu6050_filter_config_t;
 
 typedef struct {
     float x;
@@ -61,20 +67,30 @@ typedef struct {
     float z;
 } gyro_data_t;
 
-esp_err_t mpu6050_init();
-esp_err_t mpu6050_set_filter(uint8_t filter_config);
-esp_err_t mpu6050_soft_reset();
-esp_err_t mpu6050_hard_reset();
-esp_err_t mpu6050_pwr_mgmt_1_mode(uint8_t sleep, uint8_t cycle, uint8_t temp_dis, uint8_t clk_sel);
-esp_err_t mpu6050_pwr_mgmt_2_mode(uint8_t lp_wake_ctrl, uint8_t stby_zg, uint8_t stby_yg, uint8_t stby_xg, uint8_t stby_za, uint8_t stby_ya, uint8_t stby_xa);
+typedef struct {
+    uint8_t addr;
+    mpu6050_filter_config_t filter_config;
+    mpu6050_accel_fs_t accel_fs;
+    mpu6050_gyro_fs_t gyro_fs;
+    accel_data_t accel_offset;
+    gyro_data_t gyro_offset;
+    uint16_t sensor_temp;
+} mpu6050_dev_t;
 
-esp_err_t mput0650_accel_set_config(uint8_t accel_fs);
-esp_err_t mput0650_gyro_set_config(uint8_t gyro_fs);
+esp_err_t mpu6050_init(mpu6050_dev_t *dev);
+esp_err_t mpu6050_set_filter(mpu6050_dev_t *dev, mpu6050_filter_config_t filter_config);
+esp_err_t mpu6050_soft_reset(mpu6050_dev_t *dev);
+esp_err_t mpu6050_hard_reset(mpu6050_dev_t *dev);
+esp_err_t mpu6050_pwr_mgmt_1_mode(mpu6050_dev_t *dev, uint8_t sleep, uint8_t cycle, uint8_t temp_dis, uint8_t clk_sel);
+esp_err_t mpu6050_pwr_mgmt_2_mode(mpu6050_dev_t *dev, uint8_t lp_wake_ctrl, uint8_t stby_zg, uint8_t stby_yg, uint8_t stby_xg, uint8_t stby_za, uint8_t stby_ya, uint8_t stby_xa);
 
-esp_err_t mput0650_accel_set_offset(int16_t x, int16_t y, int16_t z);
-esp_err_t mput0650_gyro_set_offset(int16_t x, int16_t y, int16_t z);
+esp_err_t mpu6050_accel_set_config(mpu6050_dev_t *dev, mpu6050_accel_fs_t accel_fs);
+esp_err_t mpu6050_gyro_set_config(mpu6050_dev_t *dev, mpu6050_gyro_fs_t gyro_fs);
 
-esp_err_t mput0650_accel_read_data(accel_data_t *data);
-esp_err_t mput0650_gyro_read_data(gyro_data_t *data);
+esp_err_t mpu6050_calibrate_accel(mpu6050_dev_t *dev, uint16_t samples);
+esp_err_t mpu6050_calibrate_gyro(mpu6050_dev_t *dev, uint16_t samples);
 
-esp_err_t mpu6050_read_temp(int16_t *temp);
+esp_err_t mpu6050_accel_read_data(mpu6050_dev_t *dev, accel_data_t *data);
+esp_err_t mpu6050_gyro_read_data(mpu6050_dev_t *dev, gyro_data_t *data);
+
+esp_err_t mpu6050_read_temp(mpu6050_dev_t *dev, int16_t *temp);
