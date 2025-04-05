6bcab4444aa0df84281e7665ff8f953f2b0698f6
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-04-05 13:48:33 -0300

    fix: apagar protótipo de função que não existia no header

--- Carga/lib/I2C_sensor/I2C_sensor.h
+++ Carga/lib/I2C_sensor/I2C_sensor.h
@@ -8,5 +8,4 @@ esp_err_t sensor_read_cmd(uint8_t addr, uint8_t *cmd);
 esp_err_t sensor_read_cmd(uint8_t addr, uint8_t *cmd, size_t len);
 
 esp_err_t sensor_write_reg(uint8_t addr, uint8_t reg, uint8_t data);
-esp_err_t sensor_read_reg(uint8_t addr, uint8_t reg, uint8_t *data);
 esp_err_t sensor_read_reg(uint8_t addr, uint8_t reg, uint8_t *data, size_t len);
