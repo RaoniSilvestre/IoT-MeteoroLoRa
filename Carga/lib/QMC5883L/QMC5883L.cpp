4d173c16e6d5ebe7f79eb12838c301df6d28582e
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2025-04-10 15:35:48 -0300

    feat: Reestruturação da biblioteca e da modelagem do sensor.

--- Carga/lib/QMC5883L/QMC5883L.cpp
+++ Carga/lib/QMC5883L/QMC5883L.cpp
@@ -15,6 +15,27 @@
 #define QMC5883L_REG_CTRL_2 0x0A
 #define QMC5883L_REG_SET_RESET_PERIOD 0x0B
 
+#define QMC5883L_CTRL_1_MODE_STANDBY 0x00
+#define QMC5883L_CTRL_1_MODE_CONTINUOUS 0x01
+
+#define QMC5883L_CTRL_1_ODR_10HZ 0x00
+#define QMC5883L_CTRL_1_ODR_50HZ 0x04
+#define QMC5883L_CTRL_1_ODR_100HZ 0x08
+#define QMC5883L_CTRL_1_ODR_200HZ 0x0C
+
+#define QMC5883L_CTRL_1_RNG_2G 0x00
+#define QMC5883L_CTRL_1_RNG_8G 0x10
+
+#define QMC5883L_CTRL_1_OSR_512 0x00
+#define QMC5883L_CTRL_1_OSR_256 0x20
+#define QMC5883L_CTRL_1_OSR_128 0x40
+#define QMC5883L_CTRL_1_OSR_64 0x60
+
+#define QMC5883L_CTRL_2_INT_DIS 0x00
+#define QMC5883L_CTRL_2_INT_ENA 0x01
+#define QMC5883L_CTRL_2_ROL_PNT 0x40
+#define QMC5883L_CTRL_2_SFT_RST 0x80
+
 float _offset[] = {0, 0, 0};
 float _scale[] = {1, 1, 1};
 
@@ -147,12 +168,12 @@ esp_err_t qmc5883l_read_data(magneto_data_t *data) {
     return ESP_OK;
 }
 
-esp_err_t qmc5883l_read_temp(int16_t *temp) {
+esp_err_t qmc5883l_read_temp(float *temp) {
     uint8_t buffer[2];
     if (sensor_read_reg(QMC5883L_ADDR, QMC5883L_REG_TEMP_LSB, buffer, 2) != ESP_OK) {
         Serial.println("[QMC5883L (Magneto)] Falha ao ler temperatura");
         return ESP_FAIL;
     }
-    *temp = (int16_t)(buffer[1] << 8 | buffer[0]);
+    *temp = ((int16_t)(buffer[1] << 8 | buffer[0])) / 100;
     return ESP_OK;
 }
--- Carga/lib/QMC5883L/QMC5883L.h
+++ Carga/lib/QMC5883L/QMC5883L.h
@@ -5,32 +5,49 @@
 
 #define QMC5883L_ADDR 0x0D
 
-#define QMC5883L_CTRL_1_MODE_STANDBY 0x00
-#define QMC5883L_CTRL_1_MODE_CONTINUOUS 0x01
+typedef enum {
+    QMC5883L_CONFIG_RNG_2G = 0,
+    QMC5883L_CONFIG_RNG_8G,
+} qmc5883l_config_rng_t;
 
-#define QMC5883L_CTRL_1_ODR_10HZ 0x00
-#define QMC5883L_CTRL_1_ODR_50HZ 0x04
-#define QMC5883L_CTRL_1_ODR_100HZ 0x08
-#define QMC5883L_CTRL_1_ODR_200HZ 0x0C
+typedef enum {
+    QMC5883L_CONFIG_ODR_10HZ = 0,
+    QMC5883L_CONFIG_ODR_50HZ,
+    QMC5883L_CONFIG_ODR_100HZ,
+    QMC5883L_CONFIG_ODR_200HZ,
+} qmc5883l_config_odr_t;
 
-#define QMC5883L_CTRL_1_RNG_2G 0x00
-#define QMC5883L_CTRL_1_RNG_8G 0x10
+typedef enum {
+    QMC5883L_CONFIG_OSR_512 = 0,
+    QMC5883L_CONFIG_OSR_256,
+    QMC5883L_CONFIG_OSR_128,
+    QMC5883L_CONFIG_OSR_64,
+} qmc5883l_config_osr_t;
 
-#define QMC5883L_CTRL_1_OSR_512 0x00
-#define QMC5883L_CTRL_1_OSR_256 0x20
-#define QMC5883L_CTRL_1_OSR_128 0x40
-#define QMC5883L_CTRL_1_OSR_64 0x60
-
-#define QMC5883L_CTRL_2_INT_DIS 0x00
-#define QMC5883L_CTRL_2_INT_ENA 0x01
-#define QMC5883L_CTRL_2_ROL_PNT 0x40
-#define QMC5883L_CTRL_2_SFT_RST 0x80
+typedef enum {
+    QMC5883L_CONFIG_MODE_STANDBY = 0,
+    QMC5883L_CONFIG_MODE_CONTINUOUS,
+} qmc5883l_config_mode_t;
 
 typedef struct {
-    int16_t x;
-    int16_t y;
-    int16_t z;
+    float x;
+    float y;
+    float z;
 } magneto_data_t;
+
+typedef struct {
+    qmc5883l_config_rng_t rng_config;
+    qmc5883l_config_odr_t odr_config;
+    qmc5883l_config_osr_t osr_config;
+    qmc5883l_config_mode_t mode_config;
+} qmc5883l_config_t;
+
+typedef struct {
+    uint8_t addr;
+    qmc5883l_config_t sensor_config;
+    magneto_data_t offset, scale;
+    float sensor_temp;
+} qmc5883l_dev_t;
 
 esp_err_t qmc5883l_init();
 esp_err_t qmc5883l_set_mode(uint8_t mode, uint8_t odr, uint8_t rng, uint8_t osr);
@@ -38,4 +55,4 @@ esp_err_t qmc5883l_soft_reset();
 esp_err_t qmc5883l_calibrate();
 esp_err_t qmc5883l_read_status(uint8_t *status);
 esp_err_t qmc5883l_read_data(magneto_data_t *data);
-esp_err_t qmc5883l_read_temp(int16_t *temp);
+esp_err_t qmc5883l_read_temp(float *temp);
